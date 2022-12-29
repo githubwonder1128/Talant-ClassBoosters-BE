2094cadca48e319281c3eef6a319c09bc3fab036
Talant <crazywonderrabbit1128@gmail.com>
2022-12-29 01:35:09 -0800

    deployment

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use('/api/department', department);
 app.use('/api/course', course);
 app.use('/api/document', document)
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
