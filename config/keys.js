42445b58a28550295371d37e8879983bbe5195a4
Talant <crazywonderrabbit1128@gmail.com>
2023-01-07 06:12:51 -0800

    url

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 export const setting = {
-    // mongoURI: "mongodb+srv://course_hero:course_hero@courseherocluster.chhno.mongodb.net/classboosters?retryWrites=true&w=majority",
-    mongoURI: "mongodb://127.0.0.1:27017/fileProcessor",
+    mongoURI: "mongodb+srv://course_hero:course_hero@courseherocluster.chhno.mongodb.net/classboosters?retryWrites=true&w=majority",
+    // mongoURI: "mongodb://127.0.0.1:27017/fileProcessor",
     secretOrKey: "fileProcessor" // Add your secret string or key here
 }
